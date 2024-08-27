ed13f1cdf6c9d2b412bdaf4c788cc9e1b6393106
Fabian Scheidt <f-scheidt@gmx.de>
2024-08-27 10:25:42 +0200

    removed.

--- R_demo_scripts/test_ULA.R
+++
@@ -1,9 +0,0 @@
-load("~/Documents/R/ctlars/R_demo_scripts/ULA_test.RData")
-
-cat("Start Complex Terminating Lars\n")
-ctlars_obj <- ctlars::ctlars$new(A,
-                                 y,
-                                 has_intercept = FALSE,
-                                 num_dummies = ncol(A),
-                                 verbose = TRUE)
-ctlars_obj$execute_clars_step(t_stop = 30, early_stop = FALSE, use_chol = TRUE)
